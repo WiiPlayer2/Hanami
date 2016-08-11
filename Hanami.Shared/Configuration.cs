50f456b51ddc6c3f9806b6d502b5d95fe7d38b77
WiiPlayer2 <waldetomme@gmail.com>
2016-08-11 15:31:17 +0200

    Implemented Configuration

--- Hanami.Shared/Configuration.cs
+++ Hanami.Shared/Configuration.cs
@@ -2,43 +2,68 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.IO;
+using System.Globalization;
 
 namespace Hanami.Shared
 {
     public class Configuration
     {
-        public Configuration(System.IO.Stream stream)
+        private Dictionary<string, IConvertible> values;
+
+        public Configuration(Stream stream)
         {
-            throw new System.NotImplementedException();
+            values = new Dictionary<string, IConvertible>();
+
+            var reader = new StreamReader(stream);
+            while (!reader.EndOfStream)
+            {
+                var line = reader.ReadLine();
+
+                var splitIndex = line.IndexOf('=');
+                var name = line.Substring(0, splitIndex).Trim();
+                var val = line.Substring(splitIndex + 1).Trim();
+
+                values[name] = val;
+            }
         }
 
         public T Get<T>(string name)
             where T : IConvertible
         {
-            throw new System.NotImplementedException();
+            return (T)values[name].ToType(typeof(T), CultureInfo.InvariantCulture);
         }
 
         public T Ensure<T>(string name, T defaultValue)
             where T : IConvertible
         {
-            throw new System.NotImplementedException();
+            return Ensure<T>(name, defaultValue, _ => true);
         }
 
         public T Ensure<T>(string name, T defaultValue, Func<T, bool> checkRange)
             where T : IConvertible
         {
-            throw new System.NotImplementedException();
+            if (values.ContainsKey(name) && checkRange(Get<T>(name)))
+            {
+                return Get<T>(name);
+            }
+            values[name] = defaultValue;
+            return defaultValue;
         }
 
         public void Set<T>(string name, T value)
             where T : IConvertible
         {
-            throw new System.NotImplementedException();
+            values[name] = value;
         }
 
-        public void Save(System.IO.Stream stream)
+        public void Save(Stream stream)
         {
-            throw new System.NotImplementedException();
+            var writer = new StreamWriter(stream);
+            foreach (var kv in values)
+            {
+                writer.WriteLine("{0} = {1}", kv.Key, kv.Value);
+            }
         }
     }
 }
